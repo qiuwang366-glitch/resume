041f85e70967d7838560dc43dc9facd3573bbbcf
qiuwang366-glitch <qiuwang366@gmail.com>
2026-01-28 17:11:03 +0800

    #2 static html
    添加 output: 'export' 和关闭图片优化

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. 开启静态导出模式 (这是关键！)
+  output: "export",
+
+  // 2. 关闭默认的图片优化 (因为静态模式下没有服务器来压缩图片)
+  // 如果你不加这一行，build 的时候会报错
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
